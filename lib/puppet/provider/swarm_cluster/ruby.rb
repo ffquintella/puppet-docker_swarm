7eef679accb640eb2821a98f3430efd79bc35bae
Felipe F Quintella <felipe.quintella@fgv.br>
2016-01-27 11:28:29 -0200

    BUGFIX: Needed alteration to make orchestration ok

--- lib/puppet/provider/swarm_cluster/ruby.rb
+++ lib/puppet/provider/swarm_cluster/ruby.rb
@@ -3,39 +3,39 @@ require 'resolv'
 
 Puppet::Type.type(:swarm_cluster).provide(:ruby) do
   desc "Support for Docker Swarm"
-  
+
   mk_resource_methods
 
-  commands :swarm => "swarm"   
+  commands :swarm => "swarm"
 
   def interface
     hostname = Socket.gethostname
-    IPSocket.getaddress(hostname) 
+    IPSocket.getaddress(hostname)
   end
 
   def swarm_conf
-    cluster = resource[:cluster_type] 
+    cluster = resource[:cluster_type]
     backend = (resource[:backend])
     address = (resource[:address])
-    port = (resource[:port])  
-    advertise = (resource[:advertise])  
+    port = (resource[:port])
+    advertise = (resource[:advertise])
     path = (resource[:path])
-    case 
+    case
       when cluster.match(/create/)
         [['create']]
       when cluster.match(/join/)
         [['join', "--advertise=#{interface}:2375", "#{backend}://#{address}:#{port}/#{path}"]]
-      when cluster.match(/manage/)      
-        [['manage', '-H', "tcp://#{interface}:2376", "#{backend}://#{address}:#{port}/#{path}"], ['manage', '-H', ':4000', '--replication', '--advertise', "#{advertise}:4000", "#{backend}://#{address}:#{port}/#{path}"]] 
+      when cluster.match(/manage/)
+        [['join', "--advertise=#{interface}:2375", "#{backend}://#{address}:#{port}/#{path}"],['manage', '-H', "tcp://#{interface}:2376", "#{backend}://#{address}:#{port}/#{path}"], ['manage', '-H', ':4000', '--replication', "--advertise=#{advertise}:4000", "#{backend}://#{address}:#{port}/#{path}"]] 
       end
    end
 
    def exists?
       Puppet.info("Checking if swarm is running")
-       pid = `ps -ef | grep swarm | grep -v grep`   
+       pid = `ps -ef | grep swarm | grep -v grep`
        ! pid.length.eql? 0
    end
- 
+
    def create
      Puppet.info("Configuring Swarm Cluster")
      swarm_conf.each do |conf|
